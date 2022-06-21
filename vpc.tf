97b8487125d6782cd8bf9c8ba71bbbd659270986
Jakub Olszewski <you@example.com>
2022-06-21 23:04:54 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 module "network" {
   source  = "app.terraform.io/dzonson/network/google"
   version = "3.4.0"
-  network_name = guarav-network
+  network_name = "guarav-network"
   project_id = var.project
   subnets = [
   {
